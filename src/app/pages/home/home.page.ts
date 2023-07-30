c21e55b6ce6ac5ce98922b477d5e6331234343e5
JoseM <jmmora1974@gmail.com>
2023-07-30 22:55:37 +0200

    modified:   android/app/capacitor.build.gradle 	modified:   android/capacitor.settings.gradle 	modified:   changelog.txt 	modified:   package-lock.json 	modified:   package.json 	modified:   src/app/pages/home/home.page.ts 	modified:   workaround3.txt

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -16,7 +16,12 @@ import { map } from 'rxjs';
 import { GooglemapsService } from 'src/app/services/googlemaps.service';
 import { title } from 'process';
 import { SignupPage } from '../signup/signup.page';
-
+import {
+  ActionPerformed,
+  PushNotificationSchema,
+  PushNotifications,
+  Token,
+} from '@capacitor/push-notifications';
 
 @Component({
   selector: 'app-home',
@@ -54,6 +59,43 @@ positionSet: Marker | undefined;
 
 
   ngOnInit(): void {
+    PushNotifications.requestPermissions().then(result => {
+      if (result.receive === 'granted') {
+        // Register with Apple / Google to receive push via APNS/FCM
+        console.log('Servicio de mensajeria iniciado.');
+        PushNotifications.register();
+      } else {
+        // Show some error
+        console.log('Ha ocurrido un fallo en el servicio de mensajeria');
+      }
+    });
+    // On success, we should be able to receive notifications
+    PushNotifications.addListener('registration',
+      (token: Token) => {
+        alert('Push registration success, token: ' + token.value);
+      }
+    );
+
+    // Some issue with our setup and push will not work
+    PushNotifications.addListener('registrationError',
+      (error: any) => {
+        alert('Error on registration: ' + JSON.stringify(error));
+      }
+    );
+
+    // Show us the notification payload if the app is open on our device
+    PushNotifications.addListener('pushNotificationReceived',
+      (notification: PushNotificationSchema) => {
+        alert('Push received: ' + JSON.stringify(notification));
+      }
+    );
+
+    // Method called when tapping on a notification
+    PushNotifications.addListener('pushNotificationActionPerformed',
+      (notification: ActionPerformed) => {
+        alert('Push action performed: ' + JSON.stringify(notification));
+      }
+    );
     
   }
 
